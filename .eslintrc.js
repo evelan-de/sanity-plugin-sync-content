d5d03c3df01cb0390cb08d2be51a3543768abf1a
Gian Gonzaga <gian@giangonzaga.com>
2023-12-15 12:43:12 +0000

    chore: prettier and eslint configurations from evelan

---
+++ .eslintrc.js
@@ -0,0 +1,94 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react-hooks",
+    "simple-import-sort",
+    "unused-imports",
+    "import",
+  ],
+  extends: [
+    "sanity",
+    "sanity/typescript",
+    "sanity/react",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "@sanity/eslint-config-studio",
+  ],
+  rules: {
+     "@typescript-eslint/consistent-type-assertions": [
+      "warn",
+      {
+        assertionStyle: "never",
+      },
+    ],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        ignoreRestSiblings: true,
+      },
+    ],
+    "no-console": [
+      "error",
+      {
+        allow: ["error"],
+      },
+    ],
+     "no-restricted-imports": [
+      "error",
+      {
+        patterns: [
+          /**
+           * This pattern prevents us from using relative path imports
+           *
+           * @link https://stackoverflow.com/a/65684336
+           */
+          ".*",
+        ],
+      },
+    ],
+    "import/named": 0,
+    "import/no-unresolved": "off",
+    "import/no-cycle": [
+      2,
+      {
+        maxDepth: 10,
+      },
+    ],
+    "simple-import-sort/imports": "warn",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "unused-imports/no-unused-imports": "error",
+    "linebreak-style": ["warn", "unix"],
+  },
+  ignorePatterns: [
+    ".eslintrc.js",
+    "*.config.*",
+    "legacy/**/*",
+    "src/project/sanity/theme/*.js",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": ["off"],
+      },
+    },
+  ],
+}
