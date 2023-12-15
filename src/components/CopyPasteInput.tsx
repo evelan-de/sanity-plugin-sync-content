5abdd5fb7fa4ee3e345bd232f3f6e6a1312b876b
Gian Gonzaga <gian@giangonzaga.com>
2023-12-15 12:47:28 +0000

    chore: refactor CopyPasteInput

--- src/components/CopyPasteInput.tsx
+++ src/components/CopyPasteInput.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
-import {ClipboardIcon, CopyIcon} from '@sanity/icons'
+import { ClipboardIcon, CopyIcon } from '@sanity/icons';
 import {
   Box,
   Button,
@@ -13,197 +13,215 @@ import {
   Text,
   ThemeProvider,
   useToast,
-} from '@sanity/ui'
-import React, {useCallback, useEffect, useState} from 'react'
-import {StringInputProps, useClient, useFormValue} from 'sanity'
-import SelectedPage from 'src/components/SelectedPage'
-import {SpinnerFixed} from 'src/components/SpinnerFixed'
-import type {CheckedPage, Page} from 'src/types'
-import {deepSearchReplace, getObjectFromLs, pagesQuery, writeObjectToLs} from 'src/utils'
-import {cn} from 'src/utils'
+} from '@sanity/ui';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StringInputProps, useClient, useFormValue } from 'sanity';
+import SelectedPage from 'src/components/SelectedPage';
+import { SpinnerFixed } from 'src/components/SpinnerFixed';
+import type { CheckedPage, Page } from 'src/types';
+import {
+  cn,
+  deepSearchReplace,
+  getObjectFromLs,
+  pagesQuery,
+  writeObjectToLs,
+} from 'src/utils';
 
-const CopyPasteInput: React.FC<StringInputProps> = ({id}) => {
-  const client = useClient({apiVersion: '2021-10-21'})
-  const toast = useToast()
-  const [open, setOpen] = useState(false)
-  const [isDeselect, setDeselect] = useState(false)
-  const [pagesForMultipleCopy, setPagesForMultipleCopy] = useState<Page[]>([])
-  const [checkedPages, setCheckedPages] = useState<CheckedPage>({})
-  const [isLoadingPaste, setLoadingPaste] = useState(false)
-  const [allowedToPaste, setAllowedToPaste] = useState(false)
-  const onClose = useCallback(() => setOpen(false), [])
-  const onOpen = useCallback(() => setOpen(true), [])
-  const match = RegExp(/_key=="(\w+)"/).exec(id) ?? []
-  const extractedString = match[1]
-  const blocksName = id.split('[')[0]
-  const blocks = useFormValue([blocksName]) as {_key: string}[]
-  const documentId = useFormValue(['_id']) as string
-  const documentType = useFormValue(['_type']) as string
+const CopyPasteInput: React.FC<StringInputProps> = ({ id }) => {
+  const client = useClient({ apiVersion: '2021-10-21' });
+  const toast = useToast();
+  const [open, setOpen] = useState(false);
+  const [isDeselect, setDeselect] = useState(false);
+  const [pagesForMultipleCopy, setPagesForMultipleCopy] = useState<Page[]>([]);
+  const [checkedPages, setCheckedPages] = useState<CheckedPage>({});
+  const [isLoadingPaste, setLoadingPaste] = useState(false);
+  const [allowedToPaste, setAllowedToPaste] = useState(false);
+  const onClose = useCallback(() => setOpen(false), []);
+  const onOpen = useCallback(() => setOpen(true), []);
+  const match = RegExp(/_key=="(\w+)"/).exec(id) ?? [];
+  const extractedString = match[1];
+  const blocksName = id.split('[')[0];
+  const blocks = useFormValue([blocksName]) as { _key: string }[];
+  const documentId = useFormValue(['_id']) as string;
+  const documentType = useFormValue(['_type']) as string;
   const parent = blocks.find((block) => block._key === extractedString) as {
-    _type: string
-    _key: string
-  }
-  const parentIndex = blocks.findIndex((block) => block._key === extractedString)
+    _type: string;
+    _key: string;
+  };
+  const parentIndex = blocks.findIndex(
+    (block) => block._key === extractedString
+  );
   const duplicateDisable =
-    Object.values(checkedPages).filter((value) => value).length < 1 || isLoadingPaste
+    Object.values(checkedPages).filter((value) => value).length < 1 ||
+    isLoadingPaste;
   useEffect(() => {
     async function retrievePages() {
-      const pages = await client.fetch<Page[]>(pagesQuery, {documentType})
-      return pages
+      const pages = await client.fetch<Page[]>(pagesQuery, { documentType });
+      return pages;
     }
     retrievePages()
       .then((pages) => setPagesForMultipleCopy(pages))
-      .catch((error) => console.error(error))
+      .catch((error) => console.error(error));
     // Only allow to "Paste" if there's an block object from localstorage that's has the same "_type" as this current block
-    const searchBlock = getObjectFromLs(parent._type)
+    const searchBlock = getObjectFromLs(parent._type);
     if (searchBlock) {
-      setAllowedToPaste(true)
+      setAllowedToPaste(true);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [client, documentType])
+  }, [client, documentType]);
   const onPaste = async () => {
-    setLoadingPaste(true)
-    const objCopy = deepSearchReplace(getObjectFromLs(parent._type))
-    const nestedPath = `${blocksName}[${parentIndex}]`
-    const published = documentId.replace(/^drafts\./, '')
-    const draftsVersionExist = await client.fetch(`!(count(*[_id == 'drafts.${published}'])==0)`)
+    setLoadingPaste(true);
+    const objCopy = deepSearchReplace(getObjectFromLs(parent._type));
+    const nestedPath = `${blocksName}[${parentIndex}]`;
+    const published = documentId.replace(/^drafts\./, '');
+    const draftsVersionExist = await client.fetch(
+      `!(count(*[_id == 'drafts.${published}'])==0)`
+    );
     const updateDocument = async () => {
       await client
         .patch(`drafts.${published}`)
-        .set({[nestedPath]: objCopy})
+        .set({ [nestedPath]: objCopy })
         .commit()
         .then(() => {
-          setLoadingPaste(false)
+          setLoadingPaste(false);
           toast.push({
             status: 'success',
             title: 'Pasted successfully',
-          })
+          });
         })
         .catch((err) => {
-          setLoadingPaste(false)
-          console.error(err)
+          setLoadingPaste(false);
+          console.error(err);
           toast.push({
             status: 'error',
             title: `Something went wrong: ${
               err.details?.items
-                ? err?.details?.items.map((item: any) => item?.error?.description).join('; ')
+                ? err?.details?.items
+                    .map((item: any) => item?.error?.description)
+                    .join('; ')
                 : err?.details?.description
             }`,
-          })
-        })
-    }
+          });
+        });
+    };
     if (draftsVersionExist) {
-      await updateDocument()
+      await updateDocument();
     } else {
       await client.fetch(`*[_id == '${published}'][0]`).then(async (result) => {
-        result._id = `drafts.${published}`
-        result._updatedAt = `${new Date().toISOString().slice(0, -5)}Z`
+        result._id = `drafts.${published}`;
+        result._updatedAt = `${new Date().toISOString().slice(0, -5)}Z`;
         await client.createIfNotExists(result).then(async () => {
-          await updateDocument()
-        })
-      })
+          await updateDocument();
+        });
+      });
     }
-  }
+  };
   const onCopy = (isToast = true) => {
-    writeObjectToLs({...parent})
+    writeObjectToLs({ ...parent });
     if (isToast) {
       toast.push({
         status: 'success',
         title: 'Copied successfully',
-      })
+      });
     }
-    setAllowedToPaste(true)
-  }
+    setAllowedToPaste(true);
+  };
   const onSubmit = () => {
-    setLoadingPaste(true)
-    const objCopy = getObjectFromLs(parent._type)
-    const pagesForPatch = Object.entries(checkedPages).filter((page) => page[1])
-    let i = 0
+    setLoadingPaste(true);
+    const objCopy = getObjectFromLs(parent._type);
+    const pagesForPatch = Object.entries(checkedPages).filter(
+      (page) => page[1]
+    );
+    let i = 0;
     const patchPages = new Promise<void>((resolve) => {
       pagesForPatch.forEach(async (page) => {
-        const _id = page[0]
-        const published = _id.replace(/^drafts\./, '')
+        const _id = page[0];
+        const published = _id.replace(/^drafts\./, '');
         const draftsVersionExist = await client.fetch(
-          `!(count(*[_id == 'drafts.${published}']) == 0)`,
-        )
+          `!(count(*[_id == 'drafts.${published}']) == 0)`
+        );
         const updateDocument = async () => {
-          const copiedBlock = deepSearchReplace(objCopy)
+          const copiedBlock = deepSearchReplace(objCopy);
           await client
             .patch(`drafts.${published}`)
-            .setIfMissing({[blocksName]: []})
+            .setIfMissing({ [blocksName]: [] })
             .insert('after', `${blocksName}[-1]`, [copiedBlock])
             .commit({
               autoGenerateArrayKeys: true,
             })
             .then(() => {
-              i++
+              i++;
               if (i === pagesForPatch.length) {
-                resolve()
+                resolve();
               }
             })
             .catch(async (err) => {
-              console.error(err)
+              console.error(err);
               toast.push({
                 status: 'error',
                 title: `Something went wrong: ${
-                  err?.details?.items?.map((item: any) => item?.error?.description)?.join('; ') ||
+                  err?.details?.items
+                    ?.map((item: any) => item?.error?.description)
+                    ?.join('; ') ||
                   err?.description ||
                   JSON.stringify(err)
                 }`,
-              })
+              });
               if (i === pagesForPatch.length) {
-                resolve()
+                resolve();
               }
-            })
-        }
+            });
+        };
         if (draftsVersionExist) {
-          await updateDocument()
+          await updateDocument();
         } else {
-          await client.fetch(`*[_id == '${published}'][0]`).then(async (result) => {
-            result._id = `drafts.${published}`
-            result._updatedAt = `${new Date().toISOString().slice(0, -5)}Z`
-            await client.createIfNotExists(result).then(async () => {
-              await updateDocument()
-            })
-          })
+          await client
+            .fetch(`*[_id == '${published}'][0]`)
+            .then(async (result) => {
+              result._id = `drafts.${published}`;
+              result._updatedAt = `${new Date().toISOString().slice(0, -5)}Z`;
+              await client.createIfNotExists(result).then(async () => {
+                await updateDocument();
+              });
+            });
         }
-      })
-    })
+      });
+    });
     patchPages
       .then(() => {
-        onClose()
+        onClose();
         if (i > 0) {
           setTimeout(() => {
             toast.push({
               status: 'success',
               title: `Duplicated to ${i} pages successfully`,
-            })
-          }, 1000)
+            });
+          }, 1000);
         } else {
           toast.push({
             status: 'warning',
             title: `Duplicated to ${i} pages with some warnings`,
-          })
+          });
         }
-        setLoadingPaste(false)
-        setCheckedPages({})
+        setLoadingPaste(false);
+        setCheckedPages({});
       })
       .catch((e) => {
-        console.error(e)
-        setLoadingPaste(false)
-      })
-  }
+        console.error(e);
+        setLoadingPaste(false);
+      });
+  };
   const multipleDuplicate = () => {
-    onCopy(false)
-    onOpen()
-  }
+    onCopy(false);
+    onOpen();
+  };
   const deselectAll = () => {
-    setCheckedPages({})
-    setDeselect(true)
+    setCheckedPages({});
+    setDeselect(true);
     setTimeout(() => {
-      setDeselect(false)
-    }, 100)
-  }
+      setDeselect(false);
+    }, 100);
+  };
   return (
     <ThemeProvider theme={studioTheme}>
       <Stack space={1}>
@@ -241,7 +259,11 @@ const CopyPasteInput: React.FC<StringInputProps> = ({id}) => {
                       mode="ghost"
                       onClick={() => deselectAll()}
                       disabled={duplicateDisable}
-                      className={cn(duplicateDisable ? 'cursor-not-allowed' : 'cursor-pointer')}
+                      className={cn(
+                        duplicateDisable
+                          ? 'cursor-not-allowed'
+                          : 'cursor-pointer'
+                      )}
                     />
                     <Button
                       text={
@@ -253,14 +275,24 @@ const CopyPasteInput: React.FC<StringInputProps> = ({id}) => {
                       disabled={duplicateDisable}
                       icon={CopyIcon}
                       type="submit"
-                      className={cn(duplicateDisable ? 'cursor-not-allowed' : 'cursor-pointer')}
+                      className={cn(
+                        duplicateDisable
+                          ? 'cursor-not-allowed'
+                          : 'cursor-pointer'
+                      )}
                     />
                   </Flex>
                 </Box>
               </form>
             ) : (
               <Card padding={4}>
-                <Flex align="center" direction="row" gap={3} height="fill" justify="center">
+                <Flex
+                  align="center"
+                  direction="row"
+                  gap={3}
+                  height="fill"
+                  justify="center"
+                >
                   <Spinner muted />
                   <Text muted size={1}>
                     Loading some content…
@@ -293,13 +325,15 @@ const CopyPasteInput: React.FC<StringInputProps> = ({id}) => {
             icon={isLoadingPaste ? SpinnerFixed : ClipboardIcon}
             disabled={!allowedToPaste || isLoadingPaste}
             className={cn(
-              allowedToPaste && !isLoadingPaste ? 'cursor-pointer' : 'cursor-not-allowed',
+              allowedToPaste && !isLoadingPaste
+                ? 'cursor-pointer'
+                : 'cursor-not-allowed'
             )}
           />
         </Flex>
       </Stack>
     </ThemeProvider>
-  )
-}
-CopyPasteInput.displayName = 'CopyPasteInput'
-export default CopyPasteInput
+  );
+};
+CopyPasteInput.displayName = 'CopyPasteInput';
+export default CopyPasteInput;
